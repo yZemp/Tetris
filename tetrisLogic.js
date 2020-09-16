f97de0d3debd76a394ad0e860eaed1ca2eb30581
Zemp <yzemp@zempwirebox.station>
2020-09-16 12:07:09 +0200

    Finished frontend
    The graphic is completed, added some basic javascript for arrow event listening

--- tetrisLogic.js
+++ tetrisLogic.js
@@ -1,1 +1,68 @@
-console.log(sos.pseudoStyle);
+/////////////////////////////////////////////////////////////
+///////////////////////// VARS //////////////////////////////
+/////////////////////////////////////////////////////////////
+var keyCodeArrow;
+
+/////////////////////////////////////////////////////////////
+////////////////////// APPEARENCE ///////////////////////////
+/////////////////////////////////////////////////////////////
+
+function backOnTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+/////////////////////////////////////////////////////////////
+//////////////////////// ARROWS /////////////////////////////
+/////////////////////////////////////////////////////////////
+
+// Disable arrow scrolling
+window.addEventListener("keydown", function(e) {
+  if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+      e.preventDefault();
+  }
+}, false);
+
+function arrowClick(direction) {
+  switch (direction) {
+    case 0:
+    case 87:
+    case 38:
+      direction = "up";
+      arrowEffect(direction);
+      break;
+    case 1:
+    case 65:
+    case 37:
+      direction = "left"
+      arrowEffect(direction);
+      break;
+    case 2:
+    case 83:
+    case 40:
+      direction = "down"
+      arrowEffect(direction);
+      break;
+    case 3:
+    case 68:
+    case 39:
+      direction = "right"
+      arrowEffect(direction);
+      break;
+    default:
+      return 0;
+  }
+
+  console.log(`${direction} key was pressed`);
+}
+
+document.addEventListener("keydown", function (e) {
+  arrowClick(e.keyCode);
+});
+
+function arrowEffect(dir) {
+  document.querySelector(`#${dir}-arrow`).style.opacity = '.7';
+  setTimeout(function () {
+    document.querySelector(`#${dir}-arrow`).style.opacity = 'unset';
+  }, 200)
+}
